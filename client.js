88e669a5010665d701cd44aa12fbf59fa97d80f4
Ivo Georgiev <ivo@linvo.org>
2016-08-26 17:28:45 +0300

    client: add events

--- client.js
+++ client.js
@@ -2,6 +2,8 @@ var fetch = typeof (window) !== 'undefined' ? window.fetch : require('node-fetch
 var events = require('events')
 
 var POLL_INTERVAL = 1000
+var STATES = { NothingSpecial: 0, Opening: 1, Buffering: 2, Playing: 3, Paused: 4, Stopped: 5, Ended: 6, Error: 7 }
+var PROPS = ['volume', 'time', 'paused', 'state', 'length', 'source']
 
 function Client (url) {
   var self = new events.EventEmitter()
@@ -11,15 +13,16 @@ function Client (url) {
   var status = { } // last state received from server
   var modified = { } // properties which we've modified, that have to be pushed to the server
 
-  var timer = null // debounce timer
+  var timer = null // next sync timer
 
-  var props = ['volume', 'time', 'paused', 'state', 'length', 'source']
-  props.forEach(function (p) {
+  PROPS.forEach(function (p) {
     Object.defineProperty(self, p, {
       get: function () { return modified.hasOwnProperty(p) ? modified[p] : status[p] },
       set: function (v) {
         modified[p] = v
         resetTimer(50) // do a sync in 50ms
+        if (p === 'volume') self.emit('volumechanged')
+        if (p === 'time') self.emit('seeking')
       }
     })
   })
@@ -45,7 +48,11 @@ function Client (url) {
   }
 
   function sendEvs (old, current) {
-
+    if (current.state === STATES.Buffering) self.emit('buffering', { buf: 0 })
+    if (old.state <= STATES.Opening && current.state > STATES.Opening && current.state < STATES.Stopped) self.emit('loaded', { duration: current.length })
+    if (current.state === STATES.Error) self.emit('error', { err: current.error })
+    if (current.state !== old.state) self.emit('statechanged', { state: current.state })
+    if (current.time !== old.time) self.emit('timeupdate', { time: current.time })
   }
 
   // TODO function init() which pulls /manifest
