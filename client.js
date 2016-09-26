09becf2e6fe7d1db4a52816e1b80f7e6f8120fee
Ivo Georgiev <ivo@linvo.org>
2016-09-26 08:53:48 +0300

    don't resetTimer more than once on .stop

--- client.js
+++ client.js
@@ -27,7 +27,7 @@ function Client (url) {
   })
 
   self.play = function (src) { self.source = modified.source = src; resetTimer(50) }
-  self.stop = function () { resetTimer(50); self.source = modified.source = null;  }
+  self.stop = function () { if (! self.source) return; resetTimer(50); self.source = modified.source = null;  }
 
   function sync () {
     var p = fetch(url, { method: 'POST', body: JSON.stringify(modified), headers: { 'content-type': 'application/json' } })
