03c76a1b8ae03eb4c22cc191e2ee29d8633d3133
Ivo Georgiev <ivo@linvo.org>
2016-09-26 08:46:26 +0300

    server: now can wait for player.update() operation to update status

--- server.js
+++ server.js
@@ -5,17 +5,22 @@ function Server (player, manifest) {
     if (!req.body) return res.end(400)
 
     // TODO: manifest
-    var modifications = req.body
+    var modifications = req.body || { }
 
     Object.keys(modifications).forEach(function (k) {
       if (k === 'source') modifications.source ? player.play(modifications.source) : player.stop()
       if (modifications[k] !== player[k]) player[k] = req.body[k]
-    })
+    });
 
-    var status = { }
-    PROPS.forEach(function (k) { status[k] = player[k] })
-    res.setHeader('content-type', 'application/json')
-    res.end(JSON.stringify(status))
+    (function(next) {
+      if (player.update) player.update(next) // request fresh status for the next call
+      else next()
+    })(function() {
+      var status = { }
+      PROPS.forEach(function (k) { status[k] = player[k] })
+      res.setHeader('content-type', 'application/json')
+      res.end(JSON.stringify(status))
+    })
   }
 }
 
